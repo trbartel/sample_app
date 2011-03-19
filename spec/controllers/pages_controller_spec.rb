dde7382feb0d790f667e700b39db7fb32729b30c
Tom Bartel <trbartel01@aol.com>
2011-03-19 17:00:49 -0400

    Added an About page and set up testing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
 describe PagesController do
+    render_views
 
   describe "GET 'home'" do
+    # this line again is your text describing what you should see upon visiting home page
     it "should be successful" do
+      # the following is the first thing of code calling the HTTP Get
       get 'home'
+      # the following indicates the response should be successful with
+      # a HTTP response code or status code of 200
       response.should be_success
     end
   end
@@ -15,5 +20,11 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  
+  describe "GET 'about'" do
+      it "should be successful" do
+          get 'about'
+          response.should be_success
+      end
+  end
 end
