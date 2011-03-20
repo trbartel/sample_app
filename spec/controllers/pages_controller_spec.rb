797273b0d7a2aa6aee5c9180807ec5735a46ad38
Tom Bartel <trbartel01@aol.com>
2011-03-19 20:42:13 -0400

    Done with static pages, layout page, variable for pages, testing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
-    render_views
+  render_views
 
   describe "GET 'home'" do
-    # this line again is your text describing what you should see upon visiting home page
     it "should be successful" do
-      # the following is the first thing of code calling the HTTP Get
       get 'home'
-      # the following indicates the response should be successful with
-      # a HTTP response code or status code of 200
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -19,12 +20,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
-  
+
   describe "GET 'about'" do
-      it "should be successful" do
-          get 'about'
-          response.should be_success
-      end
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | About")
+    end
   end
 end
