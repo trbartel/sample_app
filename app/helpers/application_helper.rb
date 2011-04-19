f4f7106c3270f017b5352e685ba71743e2dbd4d2
Tom Bartel <trbartel01@aol.com>
2011-04-19 05:58:41 -0400

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+    #define a logo help for the logo local variable
+    def logo
+      logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+    end
+    
     # Return a title on a per-page basis.               # Documentation comment
     def title                                           # method (function) definition
       base_title = "Ruby on Rails Tutorial Sample App"  # variable assignment
